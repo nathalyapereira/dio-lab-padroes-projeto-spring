3fe60bab8a731ddc454e94e8ed389fe005d477f1
Nathalya Pereira <84873348+nathalyapereira@users.noreply.github.com>
2023-03-12 17:51:15 -0300

    Projeto

---
+++ src/main/java/one/digitalinnovation/gof/service/ClienteService.java
@@ -0,0 +1,17 @@
+package one.digitalinnovation.gof.service;
+
+import one.digitalinnovation.gof.model.Cliente;
+
+public interface ClienteService {
+
+	Iterable<Cliente> buscarTodos();
+
+	Cliente buscarPorId(Long id);
+
+	void adicionar(Cliente cliente);
+
+	void atualizar(Long id, Cliente cliente);
+
+	void deletar(Long id);
+
+}
